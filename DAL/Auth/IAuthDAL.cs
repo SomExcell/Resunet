e14adc94ed442457f49d453f2086b7115de418b4
SomExcell <surin.vanya15@gmail.com>
2024-06-19 13:27:53 +0500

    Add project files.

---
+++ DAL/Auth/IAuthDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Resunet.DAL.Models;
+
+namespace Resunet.DAL.Auth
+{
+    public interface IAuthDAL
+    {
+        Task<UserModel> GetUser(string email);
+        Task<UserModel> GetUser(int id);
+        Task<int> CreateUser(UserModel model);
+    }
+}
